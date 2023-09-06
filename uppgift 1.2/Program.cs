849e3d14ff46d91e9629425dcf6040a11d1ac372
dean06001 <dean06001@E-PW0234KC>
2023-09-06 10:30:00 +0200

    Add project files.

---
+++ uppgift 1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Uppgift1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine(":(");
+            Console.WriteLine("Your PC ran into a problem and needs to restart. We´re just");
+            Console.WriteLine("collecting some error info, and then we we´ll restart for you. (0%");
+            Console.WriteLine("complete)");
+            Console.WriteLine("If you would like to know morem you can search online for this error");
+            Console.ReadKey();
+        }
+    }
+}
